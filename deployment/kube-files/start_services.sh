e38e2b6d365f951ac7149dfb9775dae5074f9bf7
Simon <formalbeing@icloud.com>
2023-05-01 17:52:36 +0200

    removing deployment

--- deployment/kube-files/start_services.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f configmap.yaml & \
-kubectl apply -f persistent-volume.yaml & \
-kubectl apply -f react.yaml & \
-kubectl apply -f fastapi.yaml & \
-kubectl apply -f maria.yaml & \
-kubectl apply -f job_server.yaml & \
-kubectl apply -f job_worker.yaml & \
